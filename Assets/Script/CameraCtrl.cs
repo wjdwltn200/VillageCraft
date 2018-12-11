081c3b41c6d78854b83b7b253e85aa5655576144
wjdwltn200 <wjdwltn200@gmail.com>
2018-12-11 23:12:45 +0900

    JJS
    Cam Move

--- Assets/Script/CameraCtrl.cs
+++ Assets/Script/CameraCtrl.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour {
-
     public float moveSpeed;
+    private Transform tr;
+    private Camera cam;
 
-    private void Update()
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        moveInputKey();
+    }
+
+    void moveInputKey()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
+            tr.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            tr.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            tr.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            tr.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Z))
+            cam.orthographicSize += 1.0f;
+        if (Input.GetKey(KeyCode.X))
+            cam.orthographicSize -= 1.0f;
     }
 }
